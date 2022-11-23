e23142c5563797502e98937ec16c739eabd5efa0
Flavio Henrique <flavio.henrique@flavio.henrique>
2022-11-23 15:35:09 -0300

    Implementation 'extraiElementosDaData' function

--- src/funcoes.js
+++ src/funcoes.js
@@ -82,21 +82,22 @@ function fatorial(n) {
  */
 function equacaoDeSegundoGrau(a, b, c) {
     let delta = b*b-4*a*c
-    console.log(delta)
     let x1 = (-b + Math.sqrt(delta)) / (2*a)
     let x2 = (-b - Math.sqrt(delta)) / (2*a)
     return [x1, x2]
 }
 
 
-/**
+/**s
  * Implemente uma função que receba uma data no formato DD/MM/YYYY, 
  * extraia o dia, mês e ano, e retorne um array com:
  *   - o ANO na posição 1.
  *   - o MÊS na posição 2.
  *   - o DIA na posição 3.
  */
- function extraiElementosDaData() {}
+ function extraiElementosDaData(data) {
+    return data.split("/").reverse() 
+ }
 
 
 /**
