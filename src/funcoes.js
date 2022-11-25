c6a442dea492fc6f8dcb75a863ec37b025088129
John Soares <john.soares@john.soares>
2022-11-25 01:56:10 -0300

    function fibonacci finished

--- src/funcoes.js
+++ src/funcoes.js
@@ -124,7 +124,17 @@ function equacaoDeSegundoGrau(a,b,c) {
  *   - posição é 7: retorna o elemento 13.
  *   - posição é 8: retorna o elemento 21.
  */
-function fibonacci() {}
+function fibonacci(n) {
+    if (n==1){
+        return 1
+    }
+    if (n==2){
+        return 1        
+    } 
+    else{
+        return fibonacci(n-1) + fibonacci(n-2)
+    }
+}
 
 
 module.exports = {
